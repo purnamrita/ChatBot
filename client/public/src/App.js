66f453bffe887daa71a6d6ff589a5c7219f4a326
Purnamrita <purnamrita22@gmail.com>
2024-03-15 22:28:48 +0530

    commit done

---
+++ client/public/src/App.js
@@ -0,0 +1,86 @@
+import { useState } from "react"
+
+const App = () => {
+  const [value, setValue] = useState("")
+  const [error, setError] = useState("")
+  const [chatHistory, setChatHistory] = useState([])
+
+  const surpriseOptions = [
+    'Who won the latest Nobel Peace Prize?',
+    'Where does a pizza come from?',
+    'Who do you make a BLT sandwich?'
+  ]
+
+  const surprise = () => {
+    const randomValue = surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)]
+    setValue(randomValue)
+  }
+
+  const getResponse = async () => {
+    if(!value){
+      setError("Error! Please ask a question!")
+      return
+    }
+    try{
+      const options = {
+        method: 'POST',
+        body: JSON.stringify({
+          history: chatHistory,
+          message: value
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+      const response = await fetch('http://localhost:8000/gemini', options)
+      const data = await response.text()
+      console.log(data)
+      setChatHistory(chatHistory => [ ...chatHistory, {
+        role: "user",
+        parts: value
+      }, {
+        role: "model",
+        parts: data
+      }])
+      setValue("")
+
+    }catch(error){
+      console.log(error)
+      setError("Something went wrong! Please try again later.")
+    }
+  }
+
+  const clear = () => {
+    setValue("")
+    setError("")
+    setChatHistory([])
+  }
+
+  return (
+      <div className="app">
+        <p>What do you want to know?
+            <button className="surprise" onClick={surprise} disabled={!chatHistory}>Surprise me</button>
+        </p>
+        <div className="input-container">
+          <input
+            value={value}
+            placeholder="When is Christmas...?"
+            onChange={(e) => setValue(e.target.value)}
+          />
+          {!error && <button onClick={getResponse}>Ask me</button>}
+          {error && <button onClick={clear}>Clear</button>}
+        </div>
+        {error && <p>{error}</p>}
+        <div className="search-result">
+          {chatHistory.map((chatItem, _index) => (
+
+            <div key={_index}>
+            <p className="answer">{chatItem.role} : {chatItem.parts}</p>
+          </div>
+          ))}
+        </div>
+      </div>
+  );
+}
+
+export default App;
